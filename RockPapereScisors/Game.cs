8650b6b916681808661f36f680b8d5a0dd794106
Sivera Adam <Sivera.Adam@stud.vassboskovice.cz>
2022-10-03 14:10:51 +0200

    Přidejte soubory projektu.

---
+++ RockPapereScisors/Game.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+
+namespace RockPapereScisors
+{
+    public class Game
+    {
+        private Timer timer;
+        public player Player1 { get;}
+        public player Player2 { get;}
+        public Game(int money)
+        {
+            Player1 = new player(money);
+            Player2 = new player(money);
+            timer = new Timer(1000);
+            timer.Elapsed += Timer_Elapsed;
+
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var p1 = Player1.GetState();
+            var p2 = Player2.GetState();
+
+            if(p1 != p2)
+            {
+                if (p1 == PlayerState.Rock)
+                {
+                    if (p2 == PlayerState.Scissors)
+                    {
+                        Player1.Win();
+                        Player2.Lose();
+                    }
+                    else
+                    {
+                        Player2.Win();
+                        Player1.Lose();
+                    }
+                }
+                else if (p1 == PlayerState.Scissors)
+                {
+                    if (p2 == PlayerState.Rock)
+                    {
+                        Player2.Win();
+                        Player1.Lose();
+                    }
+                    else
+                    {
+                        Player1.Win();
+                        Player2.Lose();
+                    }
+
+                }
+                else
+                {
+                    if (p2 == PlayerState.Scissors)
+                    {
+                        Player2.Win();
+                        Player1.Lose();
+                    }
+                    else
+                    {
+                        Player1.Win();
+                        Player2.Lose();
+                    }
+
+                }
+            }
+
+            
+        }
+        public void Start()
+        {
+            timer.Start();
+        }
+    }
+}
---
+++ RockPapereScisors/player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RockPapereScisors
+{
+    public class player
+    {
+        private Random random = new Random();
+        public int Money { get; internal set; }
+        public player(int money)
+        {
+            Money = money;
+        }
+        public PlayerState GetState()
+        {
+            int value = random.Next(0, 3);
+            return (PlayerState)value;
+
+
+        }
+        public void Win()
+        {
+            Money++;
+        }
+        public void Lose()
+        {
+            Money--;
+        }
+    }
+    public enum PlayerState
+    {
+        Rock,
+        Paper,
+        Scissors
+    }
+}
